1ff359652d8b66a510f085196a6acbd37d033ca3
reaperrr <thereaperrr@aol.com>
2021-04-02 14:14:01 +0200

    Remove internal GameSpeed defaults
    And enforce setting values explicitly all the time.
    
    Additionally, require definition of a "default" gamespeed.

--- OpenRA.Game/GameSpeed.cs
+++ OpenRA.Game/GameSpeed.cs
@@ -15,9 +15,9 @@ namespace OpenRA
 {
 	public class GameSpeed
 	{
-		public readonly string Name = "Default";
-		public readonly int Timestep = 40;
-		public readonly int OrderLatency = 3;
+		public readonly string Name;
+		public readonly int Timestep;
+		public readonly int OrderLatency;
 	}
 
 	public class GameSpeeds : IGlobalModData
@@ -28,8 +28,25 @@ namespace OpenRA
 		static object LoadSpeeds(MiniYaml y)
 		{
 			var ret = new Dictionary<string, GameSpeed>();
+			var defaultExists = false;
 			foreach (var node in y.Nodes)
-				ret.Add(node.Key, FieldLoader.Load<GameSpeed>(node.Value));
+			{
+				var gs = FieldLoader.Load<GameSpeed>(node.Value);
+				if (string.IsNullOrEmpty(gs.Name))
+					throw new YamlException("At least one gamespeed doesn't define a Name!");
+				else if (gs.Timestep < 1)
+					throw new YamlException("Gamespeed {0} doesn't define a valid Timestep!".F(gs.Name));
+				else if (gs.OrderLatency < 1)
+					throw new YamlException("Gamespeed {0} doesn't define a valid OrderLatency!".F(gs.Name));
+
+				if (node.Key == "default")
+					defaultExists = true;
+
+				ret.Add(node.Key, gs);
+			}
+
+			if (!defaultExists)
+				throw new YamlException("Mod doesn't define a 'default:' gamespeed! (Note: Name can still be anything).");
 
 			return ret;
 		}
